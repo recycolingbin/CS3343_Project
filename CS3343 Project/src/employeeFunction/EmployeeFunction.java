dd3f6d30908dac1ea6810b2b11d2b605bd41acde
Dell3 <Dell3@172.28.133.198>
2025-10-03 15:10:54 +0800

    employee function v2

--- CS3343 Project/src/employeeFunction/EmployeeFunction.java
+++ CS3343 Project/src/employeeFunction/EmployeeFunction.java
@@ -1,5 +1,8 @@
 package employeeFunction;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class EmployeeFunction extends BaseFunction{
@@ -12,25 +15,81 @@ public class EmployeeFunction extends BaseFunction{
 		return instance;
 	}
 	
-	public void Request_duty() {
+	public void Request_duty(String userid) {
 		System.out.println("Please enter the day you want to request a duty:");
 		Scanner scanner = new Scanner(System.in);
 		String day = scanner.nextLine();
 		
 		System.out.println("Please enter the section you want to request on " + day + ":");
+		String section = scanner.nextLine();
+		
+		File shift = new File("Shift.txt");
+		try (Scanner fileScanner = new Scanner(shift)) {
+            boolean found = false;
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                if (line.equals(userid)) {
+                    line = fileScanner.nextLine();
+                    if (line.equals(day)) {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+            if (found) {
+                System.out.println("You already have duty on " + day + " at section " + section + ". Cannot request another duty.");
+                return;
+            }
+		
+		try (FileWriter writer = new FileWriter("Duty_Request.txt", true)) {
+			writer.write(userid + "\n" + day + "\n" + section);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		scanner.close();
 	}
 	
-	public void Request_leave() {
-		System.out.println("Please enter the day you want to request a leave:");
+	public void Request_leave(String userid) {
+		System.out.println("Please enter the day you want to request a leave (You can only request the day you have duty on):");
 		Scanner scanner = new Scanner(System.in);
 		String day = scanner.nextLine();
+		
+		File shift = new File("Shift.txt");
+		try (Scanner fileScanner = new Scanner(shift)) {
+			boolean found = false;
+			while (fileScanner.hasNextLine()) {
+				String line = fileScanner.nextLine();
+				if (line.equals(userid)) {
+					line = fileScanner.nextLine();
+					if (line.equals(day)) {
+						found = true;
+						break;
+					}
+				}
+			}
+			if (!found) {
+				System.out.println("You do not have a duty on " + day + ". Cannot request leave.");
+				return;
+			}
+			
+		}catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+		System.out.println("Please enter the section you want to request on " + day + ":");
+		String section = scanner.nextLine();
+		
+		try (FileWriter writer = new FileWriter("Leave_Request.txt", true)) {
+			writer.write(userid + "\n" + day + "\n" + section);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		scanner.close();
 	}
 	
-	public void Login_page() {
+	public void Login_page(String userid) {
 		while (true) {
 			super.View_Shift();
 			System.out.println("Please input the action you want to do: \n 1.Request Duty \n 2.Request Leave \n 3. Logout");
@@ -38,10 +97,10 @@ public class EmployeeFunction extends BaseFunction{
 			int action = scanner.nextInt();
 			switch (action) {
 			case 1:
-				Request_duty();
+				Request_duty(userid);
 				break;
 			case 2:
-				Request_leave();
+				Request_leave(userid);
 				break;
 			case 3:
 				System.out.println("Are you sure you want to logout? (Y/N)");
