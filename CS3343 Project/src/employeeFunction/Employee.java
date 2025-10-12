e3e3228c967b03a704f0c1838aaa98b715325e8f
yywong357 <yywong357-c@my.cityu.edu.hk>
2025-10-12 23:23:58 +0800

    Add Employee Class
    Constructor/Getters/Setters

---
+++ CS3343 Project/src/employeeFunction/Employee.java
@@ -0,0 +1,43 @@
+package employeeFunction;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private String role; // e.g., "Manager", "Staff"
+    private String employeeType; // e.g., "FullTime" or "PartTime"
+
+    public Employee(int employeeId, String name, String role) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.role = role;
+        this.employeeType = "FullTime";
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    
+	public String getEmployeeType() {
+		return employeeType;
+	}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+	public void setEmployeeType(String employeeType) {
+		this.employeeType = employeeType;
+	}
+}
