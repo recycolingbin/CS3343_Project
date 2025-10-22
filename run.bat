024ee58fe209cc8ce1e86b620bed655258324671
recycoling_bin <160456522+recycolingbin@users.noreply.github.com>
2025-10-22 09:44:34 +0800

    clear the folder structure

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ===============================================
-echo   Roster Management System
-echo ===============================================
-echo.
-cd /d "%~dp0"
-java -cp bin main.Main
-pause
