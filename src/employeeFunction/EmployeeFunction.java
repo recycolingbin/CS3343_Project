9e9f2c3246d2cb1396eef08a106d0eb51b632138
william <wmyuen22-c@my.cityu.edu.hk>
2025-10-26 23:09:58 +0800

    1

--- src/employeeFunction/EmployeeFunction.java
+++ src/employeeFunction/EmployeeFunction.java
@@ -77,7 +77,6 @@ public class EmployeeFunction extends BaseFunction {
         }
     }
     
-    
 	public boolean AddDuty(String userid, String date, String session) {
 		try (Scanner fileScanner = new Scanner(DUTY_REQUEST_FILE)) {
     			boolean found = false;
